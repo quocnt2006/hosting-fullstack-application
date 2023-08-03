e95073dcffce6b059e21554641e93322d0817ab6
Quoc Nguyen <quocnt2006@gmail.com>
2023-08-03 22:31:44 +0700

    update package.json deploy.sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init  --platform node.js --region us-east-1 --keyname vockey
-eb use Htfa-api-env
+eb init htfa-api --platform node.js --region us-east-1 --keyname vockey
+eb use Htfa-api-env-1
 eb deploy
